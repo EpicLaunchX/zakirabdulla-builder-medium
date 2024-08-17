07910842ebcdc65886d7e9e48dd3e82e170b8622
Zakir Abdullayev <zakirabdullayev8@gmail.com>
2024-08-17 13:52:27 +0400

    fix: lint

--- tests/test_models.py
+++ tests/test_models.py
@@ -9,11 +9,13 @@ def test_create_burger_with_all():
     assert burger.patty == "beef"
     assert str(burger) == "sesame beef ketchup lettuce,tomato"
 
+
 def test_create_burger_with_no_toppings():
     burger = Burger(bread="sesame", patty="beef", sauce="ketchup")
     assert burger.bread == "sesame"
     assert burger.patty == "beef"
     assert str(burger) == "sesame beef ketchup no toppings"
+
 
 def test_create_burger_with_no_sauce():
     burger = Burger(bread="sesame", patty="beef", toppings=["lettuce", "tomato"])
