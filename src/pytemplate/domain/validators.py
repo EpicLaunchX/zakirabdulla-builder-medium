ad2b7d873c0ec16afd2148959a99d35d1817ee66
Zakir Abdullayev <zakirabdullayev8@gmail.com>
2024-08-19 16:31:39 +0400

    fix: import-error & review comment

--- src/pytemplate/domain/validators.py
+++ src/pytemplate/domain/validators.py
@@ -3,7 +3,7 @@ from marshmallow import fields
 
 
 class BurgerSchema(marshmallow.Schema):
-    bread = fields.String()
-    patty = fields.String()
+    bread = fields.String(required=True)
+    patty = fields.String(required=True)
     sauce = fields.String(required=False)
     toppings = fields.List(fields.String(), required=False)
