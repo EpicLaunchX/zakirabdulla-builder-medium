0e0055c1deda0c170ae528df4893c6d9e903d362
Zakir Abdullayev <zakirabdullayev8@gmail.com>
2024-08-19 15:25:00 +0400

    fix: model optional

--- src/pytemplate/domain/models.py
+++ src/pytemplate/domain/models.py
@@ -5,8 +5,8 @@ from dataclasses import dataclass
 class Burger:
     bread: str
     patty: str
-    sauce: str = None
-    toppings: list[str] = None
+    sauce: str | None = None
+    toppings: list[str] | None = None
 
     def __str__(self):
         return f"{self.bread} {self.patty} {self.sauce if self.sauce else 'no sauce'} { ','.join(self.toppings) if self.toppings else 'no toppings'}"
