ecaa1a0d86f17bdc28173ec38aa2388fed5990bd
Zakir Abdullayev <zakirabdullayev8@gmail.com>
2024-08-19 17:01:45 +0400

    fix: custom validations

--- src/pytemplate/domain/validators.py
+++ src/pytemplate/domain/validators.py
@@ -1,9 +1,9 @@
 import marshmallow
-from marshmallow import fields
+from marshmallow import fields, validate
 
 
 class BurgerSchema(marshmallow.Schema):
-    bread = fields.String(required=True)
-    patty = fields.String(required=True)
-    sauce = fields.String(required=False)
+    bread = fields.Str(required=True, validate=[validate.Length(min=1), validate.OneOf(["sesame", "potato", "wheat"])])
+    patty = fields.Str(required=True, validate=[validate.Length(min=1), validate.OneOf(["beef", "chicken", "veggie"])])
+    sauce = fields.Str(required=False)
     toppings = fields.List(fields.String(), required=False)
