0c418c1c7980b03ad8c0bb2d16d149c54f46f391
Zakir Abdullayev <zakirabdullayev8@gmail.com>
2024-08-19 21:33:51 +0400

    fix: factory

--- src/pytemplate/domain/models.py
+++ src/pytemplate/domain/models.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass
 
+import marshmallow
+
+from pytemplate.domain.validators import BurgerSchema
+
 
 @dataclass
 class Burger:
@@ -10,3 +14,17 @@ class Burger:
 
     def __str__(self):
         return f"{self.bread} {self.patty} {self.sauce if self.sauce else 'no sauce'} { ','.join(self.toppings) if self.toppings else 'no toppings'}"
+
+
+def burger_factory(bread: str = None, patty: str = None, sauce: str | None = None, toppings: list[str] | None = None) -> Burger:
+    data = {
+        "bread": bread,
+        "patty": patty,
+        "sauce": sauce,
+        "toppings": toppings,
+    }
+    try:
+        validated_data = BurgerSchema().load(data)
+    except marshmallow.ValidationError as err:
+        raise marshmallow.ValidationError(err.messages) from err
+    return Burger(**validated_data)
